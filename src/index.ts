e5d5a8df098bd365a945f2c515c3c6bc8f5ffb98
colin.doak@vertigis.com <colin.doak@vertigis.com>
2023-04-03 13:58:17 -0700

    feat: TreeView Element

--- src/index.ts
+++ src/index.ts
@@ -18,3 +18,4 @@ export { default as TableElementRegistration } from "./elements/Table";
 export { default as TabsElementRegistration } from "./elements/Tabs";
 export { default as ToggleButtonElementRegistration } from "./elements/ToggleButton";
 export { default as TypographyElementRegistration } from "./elements/Typography";
+export { default as TreeElementRegistration } from "./elements/Tree";
