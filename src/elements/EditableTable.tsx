18402d0850039d21cf3d9ebc8abf5021a8783fe9
colin.doak@vertigis.com <colin.doak@vertigis.com>
2023-05-26 10:21:36 -0700

    fix: Addressed feedback - removed edit toggle - constrained input types - added column calculation - added footer style

--- src/elements/EditableTable.tsx
+++ src/elements/EditableTable.tsx
@@ -1,19 +1,15 @@
 import Box, { BoxProps } from "@vertigis/web/ui/Box";
-import Edit from "@vertigis/web/ui/icons/Edit";
 import { FormElementProps, FormElementRegistration } from "@geocortex/workflow/runtime";
-import IconButton, { IconButtonProps } from "@vertigis/web/ui/IconButton";
 import Input from "@vertigis/web/ui/Input";
-import React, { useState, useEffect, CSSProperties } from "react";
-import Save from "@vertigis/web/ui/icons/Save";
+import React, { useState, useEffect } from "react";
 import Table, { TableProps } from "@vertigis/web/ui/Table";
 import TableBody from "@vertigis/web/ui/TableBody";
 import TableCell, { TableCellProps } from "@vertigis/web/ui/TableCell";
 import TableFooter from "@vertigis/web/ui/TableFooter";
 import TableHead from "@vertigis/web/ui/TableHead";
 import TableRow from "@vertigis/web/ui/TableRow";
-import DynamicIcon from "@vertigis/web/ui/DynamicIcon/DynamicIcon";
 
-type Total = Record<string, any>;
+type FooterRow = Record<string, any>;
 type Row = Record<string, any>;
 type SettableBoxProps = Pick<BoxProps, "maxHeight" | "maxWidth">;
 type SettableTableProps = Pick<TableProps, "size" | "stickyHeader">;
@@ -27,9 +23,9 @@ interface Column {
         row: Row,
         column: Column
     ) => any;
-    includeTotal?: boolean;
+    columnCalculation?: "sum" | "average" | ((values: any) => any);
     editable: boolean;
-    type: string;
+    type: "text" | "number" | "date" | "time" | string;
     name: string;
 }
 
@@ -39,40 +35,56 @@ interface EditableTableElementProps
         SettableTableProps {
     cols: Column[];
     rows: Row[];
-    editTooltip?: string;
-    saveTooltip?: string;
-    includeTotals?: boolean;
+    editTooltip: string;
+    saveTooltip: string;
     footerLabel?: string;
-    footerStyle?: CSSProperties;
-    saveIcon?: string;
-    editIcon?: string;
     onMouseEnter?: (row: Row) => void;
     onMouseLeave?: (row: Row) => void;
-    onChange?: (data: Row[], row: Row) => void;
+    rowCalculation?: (data: Row[], row: Row) => void;
 }
 
-const IconButtonStyleOverrides: IconButtonProps["sx"] = {
-    // Hack required to override .gcx-forms.defaults
-    borderRadius: "50% !important",
-    // Hack required to match disabled list item content
-    "&.Mui-disabled": {
-        opacity: 0.38,
-    },
+//Mimic MUI header style for footer
+const TableCellStyleOverrides: TableCellProps["sx"] = {
+    fontFamily: "var(--defaultFont)",
+    lineHeight: "2.4rem",
+    display: "table-cell",
+    verticalAlign: "inherit",
+    textAlign: "left",
+    overflowWrap: "break-word",
+    backgroundColor: "var(--secondaryBackground)",
+    fontWeight: "bold",
+    fontSize: "1.4rem",
+    color: "var(--secondaryForeground)",
 };
 
-const calculateTotals = (cols: Column[], rows: Row[]): Total => {
-    const totalCols = cols.filter((x) => x.includeTotal);
-    const total: Total = {};
+const calculateFooterRow = (cols: Column[], rows: Row[]): FooterRow => {
+    const totalCols = cols.filter((x) => x.columnCalculation);
+    const total: FooterRow = {};
     for (const col of totalCols) {
-        const vals = rows.map((x) => x[col.name]);
-        const numericVals = vals.filter((x) => typeof x == "number");
-        const sum =
-            numericVals.length > 0 ? numericVals.reduce((s: number, a: number) => s + a, 0) : 0;
-        total[col.name] = sum;
+        if (col.columnCalculation) {
+            const values = rows.map((x) => x[col.name]);
+            if (col.columnCalculation === "sum") {
+                total[col.name] = calculateSum(values);
+            } else if (col.columnCalculation === "average") {
+                total[col.name] = calculateAverage(values);
+            } else {
+                total[col.name] = col.columnCalculation(values);
+            }
+        }
     }
     return total;
 };
 
+const calculateSum = (values): number => {
+    const numericVals = values.filter((x) => typeof x == "number");
+    return numericVals.length > 0 ? numericVals.reduce((s: number, a: number) => s + a, 0) : 0;
+};
+
+const calculateAverage = (values): number => {
+    const numericVals = values.filter((x) => typeof x == "number");
+    const sum = numericVals.length > 0 ? numericVals.reduce((s: number, a: number) => s + a, 0) : 0;
+    return sum / numericVals.length || 0;
+};
 /**
  * A table form element.
  * @displayName Editable Table
@@ -87,17 +99,12 @@ function EditableTableElement(props: EditableTableElementProps): React.ReactElem
         maxWidth,
         onMouseEnter,
         onMouseLeave,
-        onChange,
+        rowCalculation,
         raiseEvent,
         setValue,
         size,
-        editTooltip,
-        saveTooltip,
         stickyHeader,
         footerLabel,
-        footerStyle,
-        saveIcon,
-        editIcon,
     } = props;
     const [data, setData] = useState(rows);
     setValue(data);
@@ -105,25 +112,12 @@ function EditableTableElement(props: EditableTableElementProps): React.ReactElem
         setData(rows);
     }, [rows]);
 
-    const includeTotals = cols.some((x) => x.includeTotal);
-    const totalData: Total = calculateTotals(cols, rows);
-    const footerLbl = footerLabel ?? "Total";
-    const editIconSrc: string = editIcon ?? "edit";
-    const saveIconSrc: string = saveIcon ?? "save";
-    const editTitle = editTooltip ?? "Edit";
-    const saveTitle = saveTooltip ?? "Save";
-
-    const handleEditToggleClick = (event: React.MouseEvent, row: Row) => {
-        if (row.isEditing) {
-            raiseEvent("custom", {
-                eventType: "rowChange",
-                rowData: { rows: data, row: row },
-            });
-        }
-        row.isEditing = !row.isEditing;
-        setData([...data]);
-        setValue(data);
-    };
+    const illegalCol = cols.some((x) => ["text", "number", "date", "time"].indexOf(x.type) == -1);
+    if (illegalCol) {
+        throw new Error("Unsupported column.  Column must be of type: text, number, date or time.");
+    }
+    const includeFooter = cols.some((x) => x.columnCalculation);
+    const footerRow: FooterRow = calculateFooterRow(cols, rows);
 
     const handleMouseEnter = (event: React.MouseEvent, row: Row, column: string) => {
         raiseEvent("custom", {
@@ -146,25 +140,22 @@ function EditableTableElement(props: EditableTableElementProps): React.ReactElem
         row: Row,
         column: Column
     ) => {
-        if ((event.target as any).type === "number") {
-            const regex = /^[0-9\b]+$/;
-            if (event.target.value === "" || regex.test(event.target.value)) {
-                row[column.name] = Number(event.target.value);
-            }
-            row[column.name] = Number(event.target.value);
-        } else if (column?.parse) {
+        if (column?.parse) {
             row[column.name] = column.parse(event, row, column);
+        } else if ((event.target as any).type === "number") {
+            row[column.name] = Number(event.target.value);
         } else {
             row[column.name] = event.target.value;
         }
-        if (onChange) {
-            onChange(data, row);
+        if (rowCalculation) {
+            rowCalculation(data, row);
         }
         raiseEvent("custom", {
             eventType: "cellChange",
             rowData: { rows: data, row: row, column: column },
         });
         setData([...data]);
+        setValue(data);
     };
 
     return (
@@ -181,7 +172,7 @@ function EditableTableElement(props: EditableTableElementProps): React.ReactElem
             >
                 <TableHead>
                     <TableRow>
-                        <TableCell />
+                        {includeFooter && <TableCell />}
                         {cols.map((col) => (
                             <TableCell align={col.align} key={col.name}>
                                 {col.alias ?? col.name}
@@ -193,36 +184,8 @@ function EditableTableElement(props: EditableTableElementProps): React.ReactElem
                     {data.map((row, index) => {
                         return (
                             <TableRow hover key={index}>
-                                <TableCell align="center" sx={{ p: 0 }}>
-                                    {
-                                        <IconButton
-                                            onClick={(event) => handleEditToggleClick(event, row)}
-                                            title={row.isEditing ? saveTitle : editTitle}
-                                            sx={{
-                                                ...IconButtonStyleOverrides,
-                                            }}
-                                        >
-                                            {row.isEditing ? (
-                                                saveIcon ? (
-                                                    <DynamicIcon
-                                                        src={
-                                                            row.isEditing
-                                                                ? saveIconSrc
-                                                                : editIconSrc
-                                                        }
-                                                        tabIndex={-1}
-                                                    />
-                                                ) : (
-                                                    <Save titleAccess={saveTooltip} tabIndex={-1} />
-                                                )
-                                            ) : editIcon ? (
-                                                <DynamicIcon src={editIcon} />
-                                            ) : (
-                                                <Edit titleAccess={editTooltip} tabIndex={-1} />
-                                            )}
-                                        </IconButton>
-                                    }
-                                </TableCell>
+                                {includeFooter && <TableCell />}
+
                                 {cols.map((col, index) => {
                                     const value = row[col.name];
                                     const formattedValue = col.format ? col.format(value) : value;
@@ -244,7 +207,7 @@ function EditableTableElement(props: EditableTableElementProps): React.ReactElem
                                                 handleMouseLeave(event, row, col.name)
                                             }
                                         >
-                                            {col.editable && row.isEditing ? (
+                                            {col.editable ? (
                                                 <Input
                                                     readOnly={false}
                                                     type={col.type}
@@ -265,21 +228,25 @@ function EditableTableElement(props: EditableTableElementProps): React.ReactElem
                         );
                     })}
                 </TableBody>
-                {includeTotals && (
+                {includeFooter && (
                     <TableFooter>
                         <TableRow hover>
-                            <TableCell align="center" sx={footerStyle}>
-                                {footerLbl}
+                            <TableCell align="center" sx={TableCellStyleOverrides}>
+                                {footerLabel}
                             </TableCell>
                             {cols.map((col, index) =>
-                                totalData[col.name] ? (
-                                    <TableCell align={col.align} key={index} sx={footerStyle}>
+                                footerRow[col.name] ? (
+                                    <TableCell
+                                        align={col.align}
+                                        key={index}
+                                        sx={TableCellStyleOverrides}
+                                    >
                                         {col.format
-                                            ? col.format(totalData[col.name])
-                                            : totalData[col.name]}
+                                            ? col.format(footerRow[col.name])
+                                            : footerRow[col.name]}
                                     </TableCell>
                                 ) : (
-                                    <TableCell sx={footerStyle} />
+                                    <TableCell sx={TableCellStyleOverrides} />
                                 )
                             )}
                         </TableRow>
@@ -295,7 +262,6 @@ const EditableTableRegistration: FormElementRegistration<EditableTableElementPro
     getInitialProperties: () => ({
         cols: [],
         rows: [],
-        value: [],
     }),
     id: "EditableTable",
 };
